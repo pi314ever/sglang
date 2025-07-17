cd87d132ae3ecf57e5c409df45beab99c26b9e0f
Daniel Huang <daniel1.huang@intel.com>
2025-07-17 02:28:18 -0700

    Fix syntax issue
    Signed-off-by: Daniel Huang <daniel1.huang@intel.com>

--- python/sglang/srt/managers/mm_utils.py
+++ python/sglang/srt/managers/mm_utils.py
@@ -19,7 +19,7 @@ from sglang.srt.managers.schedule_batch import (
 )
 from sglang.srt.mem_cache.multimodal_cache import MultiModalCache
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch
-from sglang.srt.utils import flatten_nested_list, is_hpu print_warning_once
+from sglang.srt.utils import flatten_nested_list, is_hpu, print_warning_once
 from sglang.utils import logger
 
 # NOTE: Using the shared logger from sglang.utils instead of creating a module-specific logger
